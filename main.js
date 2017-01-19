835c55bf7af4a9f6748d52882daedfa00ce8229e
flieh <ruedelaflieh@gmail.com>
2017-01-19 23:32:40 +0100

    z

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 'use strict'
-let _ = require('lodash')
 let toolBox = require('./js/toolBox.js')
-let fs = require('fs')
 
 // let csvToJson = require('./js/csvToJson.js')
 let jsonRemoveProp = require('./js/jsonRemoveProp.js')
 // csvToJson('csv/test.csv', 'json/test.json')
 jsonRemoveProp('json/test.json', 'json/new.json', 'Timestamp', 'xEmail')
-fs.writeFile('json/combined.json', _.concat('json/new.json', 'json/test.json'))
-
